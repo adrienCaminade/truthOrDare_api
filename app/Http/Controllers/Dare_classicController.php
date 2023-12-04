377c1eccc4785f61130ebaff79a2d248ef91ea97
Adrien <adriencaminade.formationdev@gmail.com>
2023-12-04 16:31:39 +0100

    Little update

--- app/Http/Controllers/Dare_classicController.php
+++ app/Http/Controllers/Dare_classicController.php
@@ -7,7 +7,22 @@ use Illuminate\Http\Request;
 
 class Dare_classicController extends Controller
 {
-            /* ------------ API PART ------------ */
+
+        /**
+     * Affichage de la liste des actions classiques
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function list_dare_classic()
+    {
+
+        $dare_classic = Dare_classic::all();
+
+        return view('dare_classic.index', compact('dare_classic'));
+    }
+
+
+    /* ------------ API PART ------------ */
 
     // Accéder à la liste de toutes les actions classiques
 
